fdb50ba65987a85cca98b0f0b8ffb01a48821887
Babar <babarsmoke24@gmail.com>
2025-04-10 21:39:23 +0530

    feat : added a function for reusability

--- universalUpdate.sh
+++ universalUpdate.sh
@@ -12,18 +12,19 @@
 release_os=/etc/os-release
 logFile=/var/log/updater.log
 errorLogFile=/var/log/error.log
-#/etc/os-release shows the Config of distro
-if grep -q "Arch" $release_os; then
+check_status_error(){
   if [ $? -ne 0 ]; then
     echo "For More details look into ${errorLogFile}"
   fi
+}
+#/etc/os-release shows the Config of distro
+if grep -q "Arch" $release_os; then
+  check_status_error
   #This belong to Arch based btw
   sudo pacman -Syu 1>>$logFile 2>>$errorLogFile
 fi
 if grep -q "Ubuntu" $release_os || grep -q "Debian" $release_os; then
-  if [ $? -ne 0 ]; then
-    echo "For More details look into ${errorLogFile}"
-  fi
+  check_status_error
   (sudo apt update -y && sudo apt upgrade -y) >>$logFile 2>>$errorLogFile
 fi
 # -q means quiet mode ( We don't print anything)
